588b18b3f4ce25fe1fed9346c6fb08b240f38992
ahmad cbeılı <ahmad119966.aa@gmial.com>
2023-05-14 23:25:30 +0300

    instructions background image

--- src/instructions/Instruction.js
+++ src/instructions/Instruction.js
@@ -46,176 +46,179 @@ const FirstInstruction = ({ visible, onCancel, onConfirm }) => {
 
     return (
         <Modal visible={visible} animationType="slide">
-            <TouchableOpacity onPress={handleCancel}>
-                <View
-                    style={{
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        justifyContent: 'flex-end',
-                        marginRight: 10,
-                        paddingTop: 10,
-                    }}>
-                    <Text style={{ fontSize: 25, fontWeight: 'bold', color: 'red' }}>
-                        Skip all
-                    </Text>
-                    <Icon name="angle-right" size={30} style={{ marginLeft: 5, color: 'red' }} />
-                </View>
-            </TouchableOpacity>
-
-            {currentPage === 1 && (
-                <ImageBackground source={require('./../image/background.jpeg')} style={{ flex: 1 }}>
-                <View
-                    style={{
-                        flex: 1,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        backgroundColor: 'rgba(0, 0, 0, 0.7)',
-                    }}>
+         <ImageBackground source={require('./../image/background.jpeg')} style={{ flex: 1, width: '100%', height: '100%' }}>
+             
+            <View style={{ flex: 1, width: '100%', height: '100%',backgroundColor: 'transparent' }}>
+                 
+                <TouchableOpacity onPress={handleCancel}>
                     <View
                         style={{
-                            backgroundColor: '#fff',
-                            padding: 20,
-                            width: '100%',
-                            height: '100%',
-                            justifyContent: 'center',
+                            flexDirection: 'row',
                             alignItems: 'center',
-                            borderColor: 'red',
+                            justifyContent: 'flex-end',
+                            marginRight: 10,
+                            paddingTop: 10,
                         }}>
-                        <Image source={require('./../image/ok.jpg')} style={{ width: 250, height: 100 }} />
-                        <Text
-                            style={{
-                                fontSize: 50,
-                                fontWeight: 'bold',
-                                textAlign: 'center',
-                                marginTop: 20,
-                                color: 'red',
-                            }}>
-                            First Page
+                        <Text style={{ fontSize: 25, fontWeight: 'bold', color: 'red' }}>
+                            Skip all
                         </Text>
+                        <Icon name="angle-right" size={30} style={{ marginLeft: 5, color: 'red' }} />
                     </View>
-                </View>
-                 </ImageBackground>
-            )}
+                </TouchableOpacity>
 
-            {currentPage === 2 && (
-                <View
-                    style={{
-                        flex: 1,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        backgroundColor: 'rgba(0, 0, 0, 0.7)',
-                    }}>
+                {currentPage === 1 && (
+                    <View
+                            style={{
+                                flex: 1,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                backgroundColor: 'rgba(0, 0, 0, 0)',
+                            }}>
+                            <View
+                                style={{
+                                   
+                                    padding: 20,
+                                    width: '100%',
+                                    height: '100%',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    borderColor: 'red',
+                                }}>
+                                <Text
+                                    style={{
+                                        fontSize: 50,
+                                        fontWeight: 'bold',
+                                        textAlign: 'center',
+                                        marginTop: 20,
+                                        color: 'red',
+                                    }}>
+                                    First Page
+                                </Text>
+                            </View>
+                        </View>
+                   
+                )}
+
+                {currentPage === 2 && (
                     <View
                         style={{
-                            backgroundColor: '#fff',
-                            padding: 20,
-                            width: '100%',
-                            height: '100%',
+                            flex: 1,
                             justifyContent: 'center',
                             alignItems: 'center',
-                            borderColor: 'red',
+                            backgroundColor: 'rgba(0, 0, 0, 0)',
                         }}>
-                        <Image source={require('./../image/ok.jpg')} style={{ width: 250, height: 100 }} />
-                        <Text
+                        <View
                             style={{
-                                fontSize: 50,
-                                fontWeight: 'bold',
-                                textAlign: 'center',
-                                marginTop: 20,
-                                color: 'red',
+                               
+                                padding: 20,
+                                width: '100%',
+                                height: '100%',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                borderColor: 'red',
                             }}>
-                            Second Page
-                        </Text>
-                    </View>
-                </View>
-            )}
-
-            {currentPage === 3 && (
-                <View
-                    style={{
-                        flex: 1,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        backgroundColor: 'rgba(0, 0, 0, 0.7)',
-                    }}>
-                    <View
-                        style={{
-                            backgroundColor: '#fff',
-                            padding: 20,
-                            width: '100%',
-                            height: '100%',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            borderColor: 'red',
-                        }}>
-                        <Image source={require('./../image/ok.jpg')} style={{ width: 250, height: 100 }} />
-                        <Text
-                            style={{
-                                fontSize: 50,
-                                fontWeight: 'bold',
-                                textAlign: 'center',
-                                marginTop: 20,
-                                color: 'red',
-                            }}>
-                            Therd Page
-                        </Text>
-                    </View>
-                </View>
-            )}
-
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 20 }}>
-                {currentPage === 1 ? (
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <TouchableOpacity onPress={handleCancel}>
-                           
-                        </TouchableOpacity>
-                        
-                        <View style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: '61.5%' }}>
-                            <View style={{ backgroundColor: 'red', width: 8, height: 8, borderRadius: 5, marginRight: 5 }}></View>
-                            <View style={{ backgroundColor: '#ccc', width: 8, height: 8, borderRadius: 5, marginRight: 5 }}></View>
-                            <View style={{ backgroundColor: '#ccc', width: 8, height: 8, borderRadius: 5 }}></View>
+                           <Text
+                                style={{
+                                    fontSize: 50,
+                                    fontWeight: 'bold',
+                                    textAlign: 'center',
+                                    marginTop: 20,
+                                    color: 'red',
+                                }}>
+                                Second Page
+                            </Text>
                         </View>
                     </View>
+                )}
 
-                ) : (
-                    <TouchableOpacity onPress={handlePrevious}>
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <Icon name="angle-left" size={30} style={{ marginRight: 10, color: 'red' }} />
-                            <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'red' }}>Previous</Text>
-                            {currentPage !== 1 && currentPage !== 2 ? (
-                                <View style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: '31.5%' }}>
-                                    <View style={{ backgroundColor: '#ccc', width: 8, height: 8, borderRadius: 5, marginRight: 5 }}></View>
-                                    <View style={{ backgroundColor: '#ccc', width: 8, height: 8, borderRadius: 5, marginRight: 5 }}></View>
-                                    <View style={{ backgroundColor: 'red', width: 8, height: 8, borderRadius: 5 }}></View>
-                                </View>
-                            ) : (
-                                <View style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: '31.5%' }}>
-                                    <View style={{ backgroundColor: '#ccc', width: 8, height: 8, borderRadius: 5, marginRight: 5 }}></View>
+                {currentPage === 3 && (
+                    <View
+                        style={{
+                            flex: 1,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            backgroundColor: 'rgba(0, 0, 0, 0)',
+                        }}>
+                        <View
+                            style={{
+                                
+                                padding: 20,
+                                width: '100%',
+                                height: '100%',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                borderColor: 'red',
+                            }}>
+                            <Text
+                                style={{
+                                    fontSize: 50,
+                                    fontWeight: 'bold',
+                                    textAlign: 'center',
+                                    marginTop: 20,
+                                    color: 'red',
+                                }}>
+                                Therd Page
+                            </Text>
+                        </View>
+                    </View>
+                )}
+
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 20 }}>
+                       {currentPage === 1 ? (
+                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                <TouchableOpacity onPress={handleCancel}>
+
+                                </TouchableOpacity>
+
+                                <View style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: '61.5%' }}>
                                     <View style={{ backgroundColor: 'red', width: 8, height: 8, borderRadius: 5, marginRight: 5 }}></View>
+                                    <View style={{ backgroundColor: '#ccc', width: 8, height: 8, borderRadius: 5, marginRight: 5 }}></View>
                                     <View style={{ backgroundColor: '#ccc', width: 8, height: 8, borderRadius: 5 }}></View>
                                 </View>
-                            )}
+                            </View>
 
-                        </View>
-                    </TouchableOpacity>
-                )}
-                {currentPage === 3 ? (
-                    <TouchableOpacity onPress={handleConfirm}>
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'red' }}>Finish</Text>
-                            <Icon name="check" size={20} style={{ marginLeft: 10, color: 'red' }} />
-                        </View>
-                    </TouchableOpacity>
-                ) : (
-                    <TouchableOpacity onPress={handleNext}>
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                            <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'red' }}>Next</Text>
-                            <Icon name="angle-right" size={30} style={{ marginLeft: 10, color: 'red' }} />
-                        </View>
-                    </TouchableOpacity>
-                )}
+                        ) : (
+                            <TouchableOpacity onPress={handlePrevious}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                    <Icon name="angle-left" size={30} style={{ marginRight: 10, color: 'red' }} />
+                                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'red' }}>Previous</Text>
+                                    {currentPage !== 1 && currentPage !== 2 ? (
+                                        <View style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: '31.5%' }}>
+                                            <View style={{ backgroundColor: '#ccc', width: 8, height: 8, borderRadius: 5, marginRight: 5 }}></View>
+                                            <View style={{ backgroundColor: '#ccc', width: 8, height: 8, borderRadius: 5, marginRight: 5 }}></View>
+                                            <View style={{ backgroundColor: 'red', width: 8, height: 8, borderRadius: 5 }}></View>
+                                        </View>
+                                    ) : (
+                                        <View style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: '31.5%' }}>
+                                            <View style={{ backgroundColor: '#ccc', width: 8, height: 8, borderRadius: 5, marginRight: 5 }}></View>
+                                            <View style={{ backgroundColor: 'red', width: 8, height: 8, borderRadius: 5, marginRight: 5 }}></View>
+                                            <View style={{ backgroundColor: '#ccc', width: 8, height: 8, borderRadius: 5 }}></View>
+                                        </View>
+                                    )}
+
+                                </View>
+                            </TouchableOpacity>
+                        )}
+                        {currentPage === 3 ? (
+                            <TouchableOpacity onPress={handleConfirm}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'red' }}>Finish</Text>
+                                    <Icon name="check" size={20} style={{ marginLeft: 10, color: 'red' }} />
+                                </View>
+                            </TouchableOpacity>
+                        ) : (
+                            <TouchableOpacity onPress={handleNext}>
+                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'red' }}>Next</Text>
+                                    <Icon name="angle-right" size={30} style={{ marginLeft: 10, color: 'red' }} />
+                                </View>
+                            </TouchableOpacity>
+                        )}
+                    
+                </View>
+                
             </View>
-
+            </ImageBackground>
         </Modal>
     );
 };
--- src/smalComponent/HelpModel.js
+++ src/smalComponent/HelpModel.js
@@ -1,6 +1,7 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { Modal, View, Text, TouchableOpacity, Image } from 'react-native';
 import FirstInstruction from '../instructions/Instruction';
+import { ImageBackground } from 'react-native';
 
 
 const HelpModel = ({ visible, onCancel, onDetails, oncancelShow }) => {
@@ -11,54 +12,55 @@ const HelpModel = ({ visible, onCancel, onDetails, oncancelShow }) => {
     };
     const handleConfirm = () => {
         setModalVisible(true);
-        
+
     };
-   
+
     return (
         <Modal visible={visible} animationType="slide" transparent={true}>
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', paddingTop: 1 }}>
 
-                <View style={{ backgroundColor: '#fff', padding: 20, width: '95%', height: '85%', borderWidth: 2, borderRadius: 10, borderColor: 'red' }}>
-                    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                        <Text style={{ fontSize: 40, fontWeight: 'bold', marginBottom: 20, color: 'red' }}>How to Use!!!</Text>
-                    </View>
-                    <View style={{}}></View>
-                    <View style={{ backgroundColor: 'red', justifyContent: 'center', alignItems: 'center', borderRadius: 3 }}>
-                        <Text style={{ color: 'white', fontSize: 25, fontWeight: 'bold' }}>Fast Explenation</Text>
-                    </View>
 
-                    <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 20, paddingTop: 50 }}>First:</Text>
-                    <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 20 }}>Second:</Text>
-                    <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 20 }}>Third:</Text>
-
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>For More Details please Click </Text>
-                        <TouchableOpacity onPress={handleConfirm} style={{ flex: 1 }}>
-                            <Text style={{ fontSize: 20, color: 'red', fontWeight: 'bold' }}>Here</Text>
-                        </TouchableOpacity>
-                    </View>
-                    
-                    {modalVisible  && (
-                        <View>
-                            <FirstInstruction
-                                onCancel={onCancel}
-                                onConfirm={handleConfirm} />
+                <View style={{ backgroundColor: '#fff', padding: 2, width: '95%', height: '85%', borderWidth: 2, borderRadius: 10, borderColor: 'red' }}>
+                    <ImageBackground source={require('./../image/background.jpeg')} style={{ flex: 1, width: '100%', height: '100%' }}>
+                    <View style={{  padding: 20, width: '95%', height: '85%', borderWidth: 0, borderRadius: 10, borderColor: 'red' }}>
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <Text style={{ fontSize: 40, fontWeight: 'bold', marginBottom: 20, color: 'red' }}>How to Use!!!</Text>
                         </View>
-                    )}
+                        <View style={{}}></View>
+                        <View style={{ backgroundColor: 'red', justifyContent: 'center', alignItems: 'center', borderRadius: 3 }}>
+                            <Text style={{ color: 'white', fontSize: 25, fontWeight: 'bold' }}>Fast Explenation</Text>
+                        </View>
 
+                        <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 20, paddingTop: 50 }}>First:</Text>
+                        <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 20 }}>Second:</Text>
+                        <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 20 }}>Third:</Text>
 
-                    <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%', marginTop: 20, paddingTop: '50%' }}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Text style={{ fontSize: 18, fontWeight: 'bold' }}>For More Details please Click </Text>
+                            <TouchableOpacity onPress={handleConfirm} style={{ flex: 1 }}>
+                                <Text style={{ fontSize: 20, color: 'red', fontWeight: 'bold' }}>Here</Text>
+                            </TouchableOpacity>
+                        </View>
 
+                        {modalVisible && (
+                            <View>
+                                <FirstInstruction
+                                    onCancel={onCancel}
+                                    onConfirm={handleConfirm} />
+                            </View>
+                        )}
 
-                        <TouchableOpacity onPress={onCancel} style={{ width: 110, height: 40, backgroundColor: 'red', borderRadius: 5, justifyContent: 'center', alignItems: 'center' }}>
-                            <Text style={{ fontSize: 30, color: 'white', fontWeight: 'bold' }}>Cancel</Text>
-                        </TouchableOpacity>
-                    </View>
+                        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%', marginTop: 20, paddingTop: '50%' }}>
+                            <TouchableOpacity onPress={onCancel} style={{ width: 110, height: 40, backgroundColor: 'red', borderRadius: 5, justifyContent: 'center', alignItems: 'center' }}>
+                                <Text style={{ fontSize: 30, color: 'white', fontWeight: 'bold' }}>Cancel</Text>
+                            </TouchableOpacity>
+                            </View>
+                        </View>
+                    </ImageBackground>
                 </View>
 
-
-
             </View>
+
         </Modal>
     );
 };
